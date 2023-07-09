8bfe37321339c5fd78abda23b395d5db7f1fb528
Bethany Lai <bethanymslai@gmail.com>
2023-07-09 11:32:51 +0800

    fix scribble animation

--- Assets/Scripts/ColoringScript.cs
+++ Assets/Scripts/ColoringScript.cs
@@ -4,19 +4,13 @@ using Unity.VisualScripting;
 using UnityEngine;
 
 
-
+[RequireComponent(typeof(Animator))]
 public class ColoringScript : MonoBehaviour
 {
-    
-    public Renderer rend;
-    //public Animator animator;
-    //private Animation anim;
+    private Animator animator;
     void Start()
     {
-        rend = GetComponent<Renderer>();
-        rend.enabled = false;
-       // anim = gameObject.GetComponent<Animation>();
-        
+       animator = GetComponent<Animator>();
 
     }
     void Update()
@@ -25,27 +19,13 @@ public class ColoringScript : MonoBehaviour
         Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         mousePosition.z = Camera.main.transform.position.z + Camera.main.nearClipPlane;
         //transform.position = mousePosition;
-
         
-
         if (Input.GetKeyDown(KeyCode.Mouse0))
         {
             transform.position = mousePosition;
             transform.gameObject.GetComponent<SpriteRenderer>().color = GameManager.instance.GetColor();
-            rend.enabled = true;
-
-            //anim.Play("Drawing"); 
-            //animator.SetBool("playing", true);
-            //await Task.Delay(1000);
-
-
-        }
-    if (Input.GetKeyUp(KeyCode.Mouse0))
-        {
             
-            rend.enabled = false;
-            //animator.SetBool("playing", false);
-            
+            animator.SetTrigger("Draw");
         }
     }
     
